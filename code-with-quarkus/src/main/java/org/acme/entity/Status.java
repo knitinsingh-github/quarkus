cc4c38ba4daf035fb650a527c225cbd0d1453e66
nitinksingh <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>
2022-12-16 22:42:31 +0530

    Quarkus : MongoDb with Panache

---
+++ code-with-quarkus/src/main/java/org/acme/entity/Status.java
@@ -0,0 +1,9 @@
+package org.acme.entity;
+
+public enum Status {
+    LIVING,
+    DECEASED,
+    WORKING,
+    STUDENT,
+    NOT_APPLICABLE
+}
---
+++ code-with-quarkus/src/main/java/org/acme/repository/Status.java
@@ -0,0 +1,9 @@
+package org.acme.repository;
+
+public enum Status {
+    LIVING,
+    DECEASED,
+    WORKING,
+    STUDENT,
+    NOT_APPLICABLE
+}
